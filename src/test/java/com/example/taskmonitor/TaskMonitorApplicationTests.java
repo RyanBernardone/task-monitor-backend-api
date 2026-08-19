8b9609017ce1a52bdc5b8890049d2e8f746935c7
Ryan <ryan.bernardone@gmail.com>
2026-08-19 19:17:01 -0300

    initial commit

---
+++ src/test/java/com/example/taskmonitor/TaskMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
